b355df4a1d2ac7595d20ca2f78586698a1b3bb58
TothZsofia <tzsofia07@gmail.com>
2024-05-16 14:00:53 +0200

    Add project files.

---
+++ Weather/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weather
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<idojaras> varosok = new List<idojaras>();
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            using StreamReader sr = new StreamReader
+                (
+                path: @"..\..\..\src\idojarasok.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            while ( !sr.EndOfStream)
+            {
+                varosok.Add(new idojaras(sr.ReadLine()));
+            }
+            sr.Close();
+
+            foreach(var item in varosok)
+            {
+                listbox.Items.Add(item.Varos);
+            }
+        }
+
+        private void Hozzaadas_Click(object sender, RoutedEventArgs e)
+        {
+            if (textbox.Text != "")
+            {
+                listbox.Items.Add(textbox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Valamit írj be!");
+            }
+        }
+
+        private void Törlés_Click(object sender, RoutedEventArgs e)
+        {
+            if(listbox.SelectedItem != null)
+            {
+                listbox.Items.Remove(listbox.SelectedItem);
+            }
+        }
+
+        private void listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listbox.SelectedItem!=null)
+            {
+                int index = listbox.SelectedIndex;
+                label1.Content = varosok[index].Homerseklet;
+                label2.Content= varosok[index].Paratartalom;
+                
+            }
+        }
+    }
+}
---
+++ Weather/idojaras.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    internal class idojaras
+    {
+        public string Varos {  get; set; }
+        public double Homerseklet { get; set;}
+        public int Paratartalom { get; set; }
+       
+
+        public idojaras(string sorok)
+        {
+            var sor= sorok.Split(';');
+            Varos = sor[0];
+            Homerseklet = Convert.ToDouble(sor[1]);
+            Paratartalom = Convert.ToInt32(sor[2]);
+            
+                
+        }
+    }
+}
